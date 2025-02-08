b3ca8d5465def88cf849debf743002be9d7e6b25
WyzalDev <wyzaldev@gmail.com>
2025-02-08 17:27:57 +0300

    Add 5 spells to UI as prefabs, instantiate them with Zenject, create SpellView script, some fixes in folder structure and installers.

---
+++ Assets/magic_heroes.Client/Develop/Infrastructure/Installers/SpellsUIInstaller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using magic_heroes.Client.Develop.View;
+using UnityEngine;
+using Zenject;
+
+namespace magic_heroes.Client.Infrastructure.Installers
+{
+    public class SpellsUIInstaller : MonoInstaller
+    {
+        private const int SPELL_COUNT = 5;
+        
+        //first SPELL_COUNT spells will initializes
+        [SerializeField] private List<GameObject> _spellsParentCanvases;
+        [SerializeField] private List<Sprite> _spellSprites;
+        
+        [SerializeField] private GameObject _spellPrefab;
+        
+        public override void InstallBindings()
+        {
+            if (_spellsParentCanvases.Count < SPELL_COUNT || _spellSprites.Count < SPELL_COUNT) return;
+            for (var i = 0; i < SPELL_COUNT; i++)
+            {
+                var spellInstance =
+                    Container.InstantiatePrefabForComponent<SpellView>(_spellPrefab, _spellsParentCanvases[i].transform);
+                spellInstance.Construct(i, _spellSprites[i]);
+            }
+            
+            Debug.Log($"Instantiated {SPELL_COUNT} spells");
+        }
+    }
+}
