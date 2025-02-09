e0ba3c51f36a5b76aa1ac7694ee897deef027cec
WyzalDev <wyzaldev@gmail.com>
2025-02-09 17:08:11 +0300

    Add ClientServerAdapter for testing client, add custom http Request and Response, add DictionaryExtension for debug string

---
+++ Assets/magic_heroes.GlobalUtils/ClientConnection/ClientServerAdapter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using magic_heroes.GlobalUtils.HttpApi;
+using UnityEngine;
+
+namespace magic_heroes.GlobalUtils.ClientConnection
+{
+    public class ClientServerAdapter
+    {
+        private static ClientServerAdapter _instance;
+
+        private static Dictionary<string, string> testFields = new Dictionary<string, string>()
+        {
+            {"testfieldname", "testserializedvalue"}
+        };
+
+        public static ClientServerAdapter Instance
+        {
+            get
+            {
+                _instance ??= new ClientServerAdapter();
+                return _instance;
+            }
+        }
+
+        public Response SendRequest(Request request)
+        {
+            Debug.Log($"MsgHandler name = {request.name}, Field = {request.fields.ToDebugString()}");
+            return new Response()
+            {
+                status = 200,
+                fields = testFields
+            };
+        }
+    }
+}
