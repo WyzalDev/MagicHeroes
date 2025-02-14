6a26053801395448ce3d537c290171a591005f54
WyzalDev <wyzaldev@gmail.com>
2025-02-14 14:50:21 +0300

    add server service locator

--- Assets/magic_heroes.GlobalUtils/Lifecycle/GlobalEntryPoint.cs
+++ Assets/magic_heroes.GlobalUtils/Lifecycle/GlobalEntryPoint.cs
@@ -10,27 +10,41 @@ namespace magic_heroes.GlobalUtils.Lifecycle
         private const string LIFECYCLE_PREFAB_NAME = "LifecycleObject";
         private const string SCENE_LOADER_PREFAB_NAME = "SceneLoader";
         private const string CAMERA_PREFAB_NAME = "Camera";
+        private const string SERVER_SERVICE_LOCATOR_PREFAB_NAME = "ServerServiceLocator";
+        
         private const string LIFECYCLE_FSM_NAME = "GlobalLifeCycleFSM";
 
         private GameObject _lifecycleMonoPrefab;
         private GameObject _sceneLoaderPrefab;
         private GameObject _cameraPrefab;
+        private GameObject _serverServiceLocatorPrefab;
 
         private void Awake()
         {
             _lifecycleMonoPrefab = Resources.Load(LIFECYCLE_PREFAB_NAME) as GameObject;
             _sceneLoaderPrefab = Resources.Load(SCENE_LOADER_PREFAB_NAME) as GameObject;
             _cameraPrefab = Resources.Load(CAMERA_PREFAB_NAME) as GameObject;
+            _serverServiceLocatorPrefab = Resources.Load(SERVER_SERVICE_LOCATOR_PREFAB_NAME) as GameObject;
         }
 
         private void Start()
         {
-            //init camera and SceneLoader
+            //init SceneLoader
             var sceneLoaderInstance = Instantiate(_sceneLoaderPrefab);
+            
+            //init camera
             var cameraInstance = Instantiate(_cameraPrefab);
             DontDestroyOnLoad(cameraInstance.gameObject);
+            
+            //init server service locator
+            var serviceLocator = Instantiate(_serverServiceLocatorPrefab);
+            
+            InitializeLifecycleFsm();
+        }
 
-            //create lifecycle fsm, initialize LifeCycleMono, add states
+        //create lifecycle fsm, initialize LifeCycleMono, add states
+        private void InitializeLifecycleFsm()
+        {
             var lifecycleFsmInstance = new Fsm(LIFECYCLE_FSM_NAME);
             lifecycleFsmInstance.AddState(new GlobalBootstrapState(lifecycleFsmInstance));
             lifecycleFsmInstance.AddState(new LoadingState(lifecycleFsmInstance));
---
+++ Assets/magic_heroes.Server/src/structure/IServiceLocator.cs
@@ -0,0 +1,13 @@
+﻿namespace magic_heroes.Server.src.structure
+{
+    public interface IServiceLocator
+    {
+        void Register<T>(T service);
+        
+        bool Unregister<T>();
+        
+        T Get<T>();
+        
+        bool IsRegistered<T>();
+    }
+}
