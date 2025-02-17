40c17e78568e4a65589a205a7d4412a964e716a1
WyzalDev <wyzaldev@gmail.com>
2025-02-17 16:56:25 +0300

    Add server side entities architecture

---
+++ Assets/magic_heroes.Server/src/entities/CharacterCurrentInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace magic_heroes.Server.entities
+{
+    public class CharacterCurrentInfo
+    {
+        public string Id { get; set; }
+
+        public Player Player { get; set; }
+
+        public Character Character { get; set; }
+
+        public int HealthPoints { get; set; }
+
+        public List<ActiveEffect> ActiveEffects { get; set; }
+    }
+}
---
+++ Assets/magic_heroes.Server/src/entities/StatusEffect.cs
@@ -0,0 +1,15 @@
+﻿namespace magic_heroes.Server.entities
+{
+    public class StatusEffect
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Power { get; set; }
+
+        public int Duration { get; set; }
+
+        public bool Dispellable { get; set; }
+    }
+}
